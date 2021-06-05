7e285e488f017757c7dc5410f932ff9a45a6e869
mayank <click4mayank@gmail.com>
2021-06-05 18:00:38 +0530

    question answer 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,50 @@
 import 'package:flutter/material.dart';
+import './question.dart';
+import './answer.dart';
 
 main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  int questionIndex = 0;
-  void answerQuestion() {
-    questionIndex = questionIndex + 1;
-    print(questionIndex);
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+
+    print(_questionIndex);
     //print('Answer Choosen !');
   }
 
   @override
   Widget build(BuildContext context) {
     var questions = [
-      'What \'s your favorite color ',
+      'What \'s your favorite color ?',
       'Whats your favorite animal ?'
     ];
+    // var questions = [
+    //   {
+    //     'questionText': 'What \'s your favorite color ?',
+    //     'answer': ['Black', 'Blue', 'Gray', 'Red']
+    //   },
+    //   {
+    //     'questionText': 'Whats your favorite animal ?',
+    //     'answer': ['Rabbit', 'Cow', 'None', 'Camel']
+    //   },
+    //   {
+    //     'questionText': 'What \'s your favorite Teacher ?',
+    //     'answer': ['Dheeraj', 'Dheeraj', 'Dheeraj', 'Dheeraj']
+    //   }
+    // ];
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(
@@ -26,13 +53,18 @@ class MyApp extends StatelessWidget {
           body: Column(
             children: <Widget>[
               // first method
-              // Text(questions.elementAt(0)),
+              Text(questions.elementAt(0)),
               // 2nd method
-              Text(questions[questionIndex]),
+              // Question(questions[_questionIndex]['questionText']),
+
+              Answer(_answerQuestion),
+              Answer(_answerQuestion),
+              Answer(_answerQuestion),
+              /*
               RaisedButton(
                 child: Text('Answer 1'),
                 // named function
-                onPressed: answerQuestion,
+                onPressed: _answerQuestion,
               ),
               RaisedButton(
                 child: Text('Answer 2'),
@@ -46,6 +78,7 @@ class MyApp extends StatelessWidget {
                   print('Answer 3 chosen ');
                 },
               ),
+              */
             ],
           )),
     );
